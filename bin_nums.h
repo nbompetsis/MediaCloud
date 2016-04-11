08e6f888f49895aade576740d6a1d3c41f956cc5
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2016-04-11 13:20:52 +0300

    Bin_nums structure for the bootstrapping of the Media Cloud

---
+++ bin_nums.h
@@ -0,0 +1,10 @@
+#ifndef __BIN_NUMS__
+#define __BIN_NUMS__
+
+/*Binary numbers structure of the Media Cloud Project*/
+typedef struct {
+	int mag_num, // magic number
+	    next_id; // next identifier
+} bin_nums;
+
+#endif
